0e0d490e4346ee388f39badd615369ce47e87dc7
igorslepenkov <slepenkov.nii@yandex.by>
2022-04-22 16:19:14 +0300

    Added HW19

--- index.js
+++ index.js
@@ -1,139 +1,166 @@
-//====================================Task1==========================
-// Выведите в консоль длину массива.
+//==============================Task1====================
+//Создать любой обьект с двумя ключами и любыми значениями в них, а затем удалить ключи из обьекта. 
 
-// const colors = ['red', 'green', 'blue'];
-// console.log(colors.length);
-
-//===================================Task2===========================
-// Выведите в консоль его последний элемент вне зависимости от его длинны.
-
-// const animals = ['monkey', 'dog', 'cat'];
-// console.log(animals[animals.length-1]);
-// console.log(animals.pop());
-
-//===================================Task3===========================
-// Удалите все элементы в массиве и выведите в консоль полученный результат.
-// > Реализуйте решение двумя способами.
-
-// const numbers = [5, 43, 63, 23, 90];
-// delete numbers[0];
-// delete numbers[1];
-// delete numbers[2];
-// delete numbers[3];
-// delete numbers[4]; // С помощью delete выдаст 5 пустых ячеек
-
-// numbers.splice(0, 5); //С помощью splice выведет пустой массив []
-// console.log(numbers);
-
-//==================================Task4============================
-// + Удалите последний элемент массива, затем вместо него добавьте в массив студента `Borya`.
-// + Удалите первый элемент массива, затем вместо него добавьте в массив студента `Andrey`.
-// + Полученный результат не забудьте вывести в консоль.
-
-// const students = ['Polina', 'Dasha', 'Masha'];
-// students.pop();
-// students.push(`Andrey`);
-// console.log(students);
-
-//===================================Task5===========================
-// Выведите в консоль все элементы массива. Сначала через цикл **`for`**, затем **`for of`**.
-
-// const cats = ['Gachito', 'Tom', 'Batman'];
-// for(let i=0; i<cats.length; i++){
-//     console.log(cats[i]);
+// const user = {
+//     name:'Mike',
+//     age:36,
 // };
-// for(let cat of cats){
-//     console.log(cat);
+// delete user.name;
+// delete user.age;
+// console.log(user);
+
+//=============================Task2=====================================
+//Создать любой обьект с двумя ключами и любыми значениями в них, 
+//а затем проверить есть ли в обьекте определенный ключ и если есть вывести в консоль **true**
+
+// const user = {
+//     name:'Mike',
+//     age:36,
 // };
 
-//====================================Task6==========================
-// + Соедините два массива чисел в один.
-// + В полученном массиве попробуйте найти индекс числа **`8`**
-
-// const evenNumbers = [2, 4, 6, 8, 10]
-// const oddNumbers = [1, 3, 5, 7, 9];
-// // const arr = [...evenNumbers, ...oddNumbers]; //var1
-// const arr = evenNumbers.concat(oddNumbers);     //var2
-// console.log(arr.indexOf(8)); //3
-
-//===================================Task7===========================
-/* + Наш бинарный массив неполный, в нем явно не хватает единиц.
- + Превратите данный массив в строку.
- > [0, 0, 0, 0] -> '0101010' */
-
-// const binary = [0, 0, 0, 0];
-// const str = binary.join('1');
-// console.log(str);
-
-//=================================Advanced Task1====================
-//Реализуйте функцию которая будет проверять, является ли слово палиндромом.
-
-// const str = 'Anna';
-// function isPolindrom(string){
-//     return string.toLowerCase() === string.toLowerCase().split('').reverse().join('');
+// if(user.name){
+//     console.log(user.name, user.age);
 // };
-// console.log(isPolindrom(str));
+// if(user.hasOwnProperty('age')){
+//     console.log(user.name);
+// };
 
-//================================Advanced Task2========================
-//Выведите в консоль среднее значение чисел в многомерном массиве.
+//============================Task3=============================================
+//Дан обьект:
+    // const student = {
+    //     name: 'John',
+    //     age: 19,
+    //     isHappy: true
+    // }
+//C помощью цикла **for in** вывести в консоль сначала все ключи, потом значения ключей обьекта.
 
-// const matrix = [
-//     [12, 98, 78, 65, 23],
-//     [54, 76, 98, 43, 65],
-//     [13, 324, 65, 312],
-//     [9092, 22, 45, 90000],
-// ];
+// for(let key in student){
+//     console.log(key);
+//     console.log(student[key]);
+// };
 
-// const matrix2 = [
-//     [[12], 98, 78, 65, 23],
-//     [[54, [76]], 98, 43, 65],
-//     [13, [324, [65, 312]]],
-//     [9092, [22, 45], [[[90000]]]],
-// ];
+//===============================Task4==============================================
+//Дан обьект:
+// const colors = {
+//     'ru pum pu ru rum': {
+//         red: 'красный',
+//         green: 'зеленый',
+//         blue: 'синий'
+//     },
+// };
+//Вывести в консоль слово красный и синий
 
-// //Рекурсия будет работать на любых уровнях вложенности
+// console.log(colors['ru pum pu ru rum'].red);
+// console.log(colors['ru pum pu ru rum'].blue);
 
-// function getMedium(array){
-//     const newArray = [];
-//     const recursion = (arr) =>{
-//         arr.forEach((el)=>{
-//             if(!Array.isArray(el)){
-//                 newArray.push(el);
-//             } else {
-//                 recursion(el);
+//=============================================Task5=====================================
+//Дан обьект:
+    // let salaries = {
+    //     andrey: 500,
+    //     sveta: 413,
+    //     anton: 987,
+    //     andrey: 664,
+    //     alexandra: 199
+    // }
+//Вычислите среднюю зарплату сотрудников и результат поместите в соответствующую переменную
+
+// let sum = 0;
+// for(let person in salaries){
+//     sum += salaries[person];
+// };
+// console.log(sum);
+
+//==========================================Task6========================================
+//Создать валидатор, запросить у пользователя логин и пароль для регистрации. Затем данные записать в обьект. 
+//Потом попросить пользователя подтвердить данные. Если верно введен логин и пароль, вывести сообщение **Добро пожалоВать**. 
+// class Validator{
+//     constructor(){
+//         this.users=[];
+//         this.createUser = () =>{
+//             const user = {
+//                 login: prompt('Create new login'),
+//                 password: prompt('Create new password'),
 //             };
-//         });
+//             this.users.push(user);
+//         };
+//         this.checkUser = () =>{
+//             let data = [];
+//             data.push(prompt('Enter your login'));
+//             data.push(prompt('Enter your password'));
+//             if(this.users.find((user)=>user.login===data[0] && user.password===data[1])){
+//                 alert('Welcome!');
+//             } else {
+//                 alert('You have passed incorrect data');
+//             };
+//         };
 //     };
-//     recursion(array);
-//     const result = (newArray.reduce((sum, current)=>sum+current))/newArray.length;
-//     return result;
 // };
-// console.log(getMedium(matrix)); // 5582.5
-// console.log(getMedium(matrix2)); // 5582.5
 
-//=======================================Advanced Task3======================================
-/*Дан массив, cоздайте два массива, в один поместите все 
-положительные числа включая 0, в другой все отрицательные. 
-Оба массива затем выведите в консоль. */
+// const validator = new Validator();
+// validator.createUser();
+// validator.checkUser();
 
-// const mixedNumbers = [-14, 24, -89, 43, 0 , -1, 412, 4];
-// const arrPositive = mixedNumbers.filter((num)=>num<0);
-// const arrNegative = mixedNumbers.filter((num)=>num>=0);
-// console.log(arrPositive, arrNegative);
+//========================================Advanced Task1=========================================
+//Мы на футбольном матче, счет забитых голов одной команды не может превышать 9 мячей. 
+//Жаль что нам присылают результат матча в формате '2:5', ведь нам надо это вывести в консоль словами. 
+//Давайте напишем программу которая будет за нас переводить формат и выводить результат в консоль. 
 
-//======================================Advanced task4=========================================
-/*Создать массив длинной не менее 5, из динамически созданных случайных чисел. 
-Далее написать алгоритм который берет все числа из исходного массива, 
-возводит их в куб и записывает в новый массив. В конце вывести оба массива в консоль.*/
-
-// const arr = [];
-// for(let i=0; i<5; i++){
-//     arr.push(Math.round(Math.random()*100));
+// function displayResults(str){
+//     const resArr = str.split(':');
+//     const translateHelper = {
+//         '1':'Один',
+//         '2':'Два',
+//         '3':'Три',
+//         '4':'Четыре',
+//         '5':'Пять',
+//         '6':'Шесть',
+//         '7':'Семь',
+//         '8':'Восемь',
+//         '9':'Девять',
+//     };
+//     console.log(`Матч окончился со счетом\n${translateHelper[resArr[0]]} : ${translateHelper[resArr[1]]}`);
 // };
-// const newArr = arr.map((num)=>num**3);
-// console.log(arr, newArr);
+// displayResults('2:5');
 
-//======================================Codewars solutions========================================
-// https://www.codewars.com/users/IgorSlepenkov/completed_solutions
+//=====================================Advanced task2=============================================
 
+// Даны два одинаковых обьекта. Сравните их так чтобы они были равны
 
+//     let student1 = {
+//         name: 'Polina',
+//         age: 27,
+//     }
+
+//     let student2 = {
+//         name: 'Polina',
+//         age: 27,
+//     }
+
+// const isEqual = (obj1, obj2) => {
+//     let bool = true;
+//     for (let key in obj1) {
+//         if (!bool) { return bool };
+//         bool = (obj1[key] === obj2[key]);
+//     };
+//     return bool;
+// };
+
+//     console.log(isEqual(student1, student2));
+
+//=====================================Advanced Task3================================================
+//У нас есть обьект с животными. Наша задача узнать имя птицы и вывести его в консоль. 
+//Но произошла ошибка и в этом обьекте нету птицы. Если попробуем узнать имя мы получим **ОШИБКУ**. 
+//Задача придумать как обратиться к несуществующему обьекту и не получить ошибку чтобы наша программа работала дальше.
+
+// const animals = {
+//     cat: {
+//        name: 'Енчик',
+//        age: 3,
+//     },
+//     dog: {
+//        name: 'Орео',
+//        age: 2,
+//     }
+//  }
+
+//  console.log(animals?.bird); // undefind
